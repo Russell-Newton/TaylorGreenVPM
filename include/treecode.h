c72c35097f15b7ff5ebb1c6757baa28a5a26bd83
Shreyas Ashok <sashok@gatech.edu>
2023-04-15 11:23:01 -0400

    treecode building without pointers

--- include/treecode.h
+++ include/treecode.h
@@ -1,25 +1,30 @@
 #pragma once
 
 #include <utility>
+#include <list>
 
 #include "vpm.h"
 
 namespace vpm {
     struct QuadTreeCodeNode {
-        QuadTreeCodeNode *q1Child;
-        QuadTreeCodeNode *q2Child;
-        QuadTreeCodeNode *q3Child;
-        QuadTreeCodeNode *q4Child;
+        size_t q1ChildIdx;
+        size_t q2ChildIdx;
+        size_t q3ChildIdx;
+        size_t q4ChildIdx;
         Particle particle;
         double size;
         bool isLeaf;
         bool isEmpty;
 
-        QuadTreeCodeNode() : q1Child(nullptr), q2Child(nullptr), q3Child(nullptr), q4Child(nullptr), particle(), size(0), isLeaf(false), isEmpty(true) {};
+        QuadTreeCodeNode() : q1ChildIdx(-1), q2ChildIdx(-1), q3ChildIdx(-1), q4ChildIdx(-1), particle(), size(0), isLeaf(false), isEmpty(true) {};
 
         virtual ~QuadTreeCodeNode();
 
-        void build(const std::vector<Particle>& particles, double _size, double _centerX, double _centerY);
+        void build(const std::vector<Particle>& particles, double _size, double _centerX, double _centerY, std::list<QuadTreeCodeNode>& tree);
+
+        friend std::ostream& operator<<(std::ostream& os, const QuadTreeCodeNode& node) {
+            return os << "Particle: " << node.particle << " Size: " << node.size << " isLeaf: " << node.isLeaf << " isEmpty: " << node.isEmpty << " q1ChildIdx: " << node.q1ChildIdx << " q2ChildIdx: " << node.q2ChildIdx << " q3ChildIdx: " << node.q3ChildIdx << " q4ChildIdx: " << node.q4ChildIdx;
+        }
     };
 
     std::vector<std::tuple<double, double, double>> CalcDerivativeTreeCode(
--- src/treecode.cpp
+++ src/treecode.cpp
@@ -16,33 +16,39 @@ std::vector<std::tuple<double, double, double>> vpm::CalcDerivativeTreeCode(
     double ParticleRadius2 = ParticleRadius * ParticleRadius;
     double ParticleVol = ParticleRadius2 * M_PI;
 
-    vpm::QuadTreeCodeNode tree = vpm::QuadTreeCodeNode();
-    tree.build(Particles, DomainL, DomainL / 2, DomainL / 2);
+    std::list<vpm::QuadTreeCodeNode> tree = std::list<vpm::QuadTreeCodeNode>();
+    vpm::QuadTreeCodeNode root = vpm::QuadTreeCodeNode();
+    tree.push_back(root);
+    // root.build(Particles, DomainL, DomainL / 2, DomainL / 2, tree);
+    tree.front().build(Particles, DomainL, DomainL / 2, DomainL / 2, tree);
+
+    std::vector<vpm::QuadTreeCodeNode> treeAsVec = std::vector<vpm::QuadTreeCodeNode>(tree.begin(), tree.end());
 
     for (size_t i = 0; i < N; i++) {
         vpm::Particle thisParticle = Particles[i];
         // get contribution from tree
-        std::stack<QuadTreeCodeNode*> toCheck = std::stack<QuadTreeCodeNode*>();
-        toCheck.push(&tree);
+        std::stack<size_t> toCheck = std::stack<size_t>();
+        toCheck.push(0);
 
         while (!toCheck.empty()) {
-            QuadTreeCodeNode* checking = toCheck.top();
+            QuadTreeCodeNode checking = treeAsVec[toCheck.top()];
             toCheck.pop();
+            // std::cout << i << " " << checking << std::endl;
 
             // ignore empty nodes
-            if (checking->isEmpty) continue;
+            if (checking.isEmpty) continue;
 
 
             // explore further if needed
             double periodicDistance[2];
-            thisParticle.PeriodicDistanceVector(checking->particle, DomainL, periodicDistance);
+            thisParticle.PeriodicDistanceVector(checking.particle, DomainL, periodicDistance);
             double periodicDist = sqrt(periodicDistance[0] * periodicDistance[0] + periodicDistance[1] * periodicDistance[1]);
-            if (checking->isLeaf && periodicDist <= 1e-10) continue;
-            if (!checking->isLeaf && checking->size / periodicDist >= OpeningAngle) {
-                toCheck.push(checking->q1Child);
-                toCheck.push(checking->q2Child);
-                toCheck.push(checking->q3Child);
-                toCheck.push(checking->q4Child);
+            if (checking.isLeaf && periodicDist <= 1e-10) continue;
+            if (!checking.isLeaf && checking.size / periodicDist >= OpeningAngle) {
+                toCheck.push(checking.q1ChildIdx);
+                toCheck.push(checking.q2ChildIdx);
+                toCheck.push(checking.q3ChildIdx);
+                toCheck.push(checking.q4ChildIdx);
                 continue;
             }
 
@@ -53,27 +59,27 @@ std::vector<std::tuple<double, double, double>> vpm::CalcDerivativeTreeCode(
                     Kernel(Rho) * periodicDistance[1] / (periodicDist * periodicDist),
                     0
             };
-            auto [ResultX, ResultY, _] = Cross(Vector1, {0, 0, checking->particle.Vorticity});
+            auto [ResultX, ResultY, _] = Cross(Vector1, {0, 0, checking.particle.Vorticity});
             std::get<0>(Out[i]) -= ResultX;
             std::get<1>(Out[i]) -= ResultY;
-            std::get<2>(Out[i]) += (2.0 * Viscosity / ParticleRadius2) * (1.0 / ParticleRadius2) * ViscousKernel(Rho) * ParticleVol * (checking->particle.Vorticity - thisParticle.Vorticity);
+            std::get<2>(Out[i]) += (2.0 * Viscosity / ParticleRadius2) * (1.0 / ParticleRadius2) * ViscousKernel(Rho) * ParticleVol * (checking.particle.Vorticity - thisParticle.Vorticity);
         }
     }
 
     return Out;
 }
 
-void vpm::QuadTreeCodeNode::build(const std::vector<Particle>& particles, double _size, double _centerX, double _centerY) {
-    this->size = _size;
+void vpm::QuadTreeCodeNode::build(const std::vector<Particle>& particles, double _size, double _centerX, double _centerY, std::list<QuadTreeCodeNode>& tree) {
+    size = _size;
     if (particles.empty()) {
-        this->isLeaf = true;
+        isLeaf = true;
         return;
     }
 
-    this->isEmpty = false;
+    isEmpty = false;
     if (particles.size() == 1) {
-        this->particle = particles[0];
-        this->isLeaf = true;
+        particle = particles[0];
+        isLeaf = true;
         return;
     }
 
@@ -94,7 +100,7 @@ void vpm::QuadTreeCodeNode::build(const std::vector<Particle>& particles, double
         centroidY /= centerVorticity;
     }
 
-    this->particle = Particle(centroidX, centroidY, 0, 0, centerVorticity);
+    particle = Particle(centroidX, centroidY, 0, 0, centerVorticity);
 
     std::vector<Particle> particlesQ1 = std::vector<Particle>();
     std::vector<Particle> particlesQ2 = std::vector<Particle>();
@@ -107,23 +113,27 @@ void vpm::QuadTreeCodeNode::build(const std::vector<Particle>& particles, double
         else if (_particle.PositionX >= _centerX && _particle.PositionY < _centerY) particlesQ4.push_back(_particle);
     }
 
-    q1Child = new vpm::QuadTreeCodeNode();
-    q2Child = new vpm::QuadTreeCodeNode();
-    q3Child = new vpm::QuadTreeCodeNode();
-    q4Child = new vpm::QuadTreeCodeNode();
-
     double subSize = _size / 2;
     double subCenterOffset = subSize / 2;
 
-    q1Child->build(particlesQ1, subSize, _centerX + subCenterOffset, _centerY + subCenterOffset);
-    q2Child->build(particlesQ2, subSize, _centerX - subCenterOffset, _centerY + subCenterOffset);
-    q3Child->build(particlesQ3, subSize, _centerX - subCenterOffset, _centerY - subCenterOffset);
-    q4Child->build(particlesQ4, subSize, _centerX + subCenterOffset, _centerY - subCenterOffset);
+    q1ChildIdx = tree.size();
+    tree.push_back({});
+    tree.back().build(particlesQ1, subSize, _centerX + subCenterOffset, _centerY + subCenterOffset, tree);
+    q2ChildIdx = tree.size();
+    tree.push_back({});
+    tree.back().build(particlesQ2, subSize, _centerX - subCenterOffset, _centerY + subCenterOffset, tree);
+    q3ChildIdx = tree.size();
+    tree.push_back({});
+    tree.back().build(particlesQ3, subSize, _centerX - subCenterOffset, _centerY - subCenterOffset, tree);
+    q4ChildIdx = tree.size();
+    tree.push_back({});
+    tree.back().build(particlesQ4, subSize, _centerX + subCenterOffset, _centerY - subCenterOffset, tree);
+
+    // tree[q1ChildIdx].build(particlesQ1, subSize, _centerX + subCenterOffset, _centerY + subCenterOffset, tree);
+    // tree[q2ChildIdx].build(particlesQ2, subSize, _centerX - subCenterOffset, _centerY + subCenterOffset, tree);
+    // tree[q3ChildIdx].build(particlesQ3, subSize, _centerX - subCenterOffset, _centerY - subCenterOffset, tree);
+    // tree[q4ChildIdx].build(particlesQ4, subSize, _centerX + subCenterOffset, _centerY - subCenterOffset, tree);
 }
 
 vpm::QuadTreeCodeNode::~QuadTreeCodeNode() {
-    delete q1Child;
-    delete q2Child;
-    delete q3Child;
-    delete q4Child;
 }
